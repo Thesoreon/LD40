b1ebff245d1fbc52fbe49bfccc1c01f2a67e149f
Thesoreon <pavels.sus@gmail.com>
2017-12-02 09:30:00 +0100

    - Added movement Scripts
    - Added Controller script for inputs from player
    - Testing sprite for player
    - Testing environment

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private PlayerMovement movement;
+
+	void Start () {
+        movement = GetComponent<PlayerMovement>();
+	}
+	
+	void FixedUpdate () {
+
+        if (Input.GetKey(KeyCode.A))
+            movement.Move(-150);
+        else if (Input.GetKey(KeyCode.D))
+            movement.Move(150);
+
+        if (Input.GetKey(KeyCode.Space))
+            movement.Jump(150);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private bool grounded;
+
+    private Rigidbody2D rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+	public void Move(float speed)
+    {
+        rb.velocity = new Vector2(speed * Time.deltaTime, rb.velocity.y);
+    }
+
+    public void Jump(float force)
+    {
+        if (grounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, force * Time.deltaTime);
+            grounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+            grounded = true;
+    }
+
+}
