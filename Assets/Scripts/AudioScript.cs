d5a37d8ba82cd74dd37458a8516a59e0af20320a
Thesoreon <pavels.sus@gmail.com>
2017-12-03 20:23:38 +0100

    - Last commit

---
+++ Assets/Scripts/AudioScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class AudioScript : MonoBehaviour {
+
+    private bool open = true;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            if (open)
+                gameObject.GetComponent<AudioSource>().enabled = false;
+            else
+                gameObject.GetComponent<AudioSource>().enabled = true;
+
+            open = !open;
+        }
+    }
+}
